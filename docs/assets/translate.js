33568e72c8ce5b4d0d3e6a17169d393ff6ec4371
Beilai Technology <304385660+Beilai-Technology@users.noreply.github.com>
2026-07-15 11:02:06 +0800

    Update translate.js

--- docs/assets/translate.js
+++ docs/assets/translate.js
@@ -1,4 +1,5 @@
 function googleTranslateElementInit() {
+
     new google.translate.TranslateElement(
         {
             pageLanguage: 'zh-CN',
@@ -7,17 +8,5 @@ function googleTranslateElementInit() {
         },
         'google_translate_element'
     );
+
 }
-
-
-document.addEventListener("DOMContentLoaded", function () {
-
-    if (!document.getElementById("google_translate_element")) {
-
-        var div = document.createElement("div");
-        div.id = "google_translate_element";
-        document.body.appendChild(div);
-
-    }
-
-});
